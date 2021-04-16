30ad43b028c2e04ede5b710cd5bc274707cba9b5
soo-oon <70465246+soo-oon@users.noreply.github.com>
2021-04-16 22:16:35 +0900

    레귤러 사이즈 제작

--- web_practise/lib/pages/main_page.dart
+++ web_practise/lib/pages/main_page.dart
@@ -6,121 +6,12 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    final double horizontalPadding = size.width * 0.08;
-    final double verticalPadding = size.height * 0.02;
-
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
           width: size.width,
-          height: 500,
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("assets/images/machine.jpg"),
-                      fit: BoxFit.cover),
-                ),
-                padding: EdgeInsets.symmetric(
-                  horizontal: horizontalPadding,
-                  vertical: verticalPadding,
-                ),
-              ),
-              Stack(
-                children: [
-                  Container(
-                    color: Colors.black.withOpacity(0.6),
-                  ),
-                  Container(
-                    alignment: Alignment.topCenter,
-                    padding: EdgeInsets.symmetric(
-                      horizontal: horizontalPadding,
-                      vertical: verticalPadding,
-                    ),
-                    child: const _Header(),
-                  ),
-                  Container(
-                    alignment: Alignment.center,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: horizontalPadding,
-                          vertical: verticalPadding),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            flex: 1,
-                            child: Text(
-                              "New Place 으으으",
-                              style: TextStyle(
-                                  fontSize: 48.0, color: Colors.white),
-                            ),
-                          ),
-                          Expanded(
-                              flex: 1,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "더 많은 기회를 노린다.",
-                                    style: TextStyle(
-                                        fontSize: 16.0, color: Colors.white),
-                                  ),
-                                  const SizedBox(height: 24.0),
-                                  ElevatedButton(
-                                    onPressed: () {},
-                                    child: Text("자세히 보기"),
-                                    style: ElevatedButton.styleFrom(
-                                        primary: Colors.black),
-                                  )
-                                ],
-                              )),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    alignment: Alignment.bottomCenter,
-                    padding: EdgeInsets.symmetric(
-                        horizontal: horizontalPadding,
-                        vertical: verticalPadding),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: const SizedBox(),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.location_on,
-                                color: Colors.white,
-                                size: 16.0,
-                              ),
-                              const SizedBox(
-                                width: 8.0,
-                              ),
-                              Text(
-                                "대구광역시 달서구 호림동",
-                                style: TextStyle(
-                                    fontSize: 16.0, color: Colors.white),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
+          height: 800,
+          child: const _RegularVersion(),
         ),
       ),
     );
@@ -198,9 +89,117 @@ class _Header extends StatelessWidget {
 }
 
 class _RegularVersion extends StatelessWidget {
+  const _RegularVersion();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    final double horizontalPadding = size.width * 0.08;
+    final double verticalPadding = size.height * 0.02;
+
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/machine.jpg"),
+                fit: BoxFit.cover),
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: horizontalPadding,
+            vertical: verticalPadding,
+          ),
+        ),
+        Container(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.black.withOpacity(0.6),
+              ),
+              Container(
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.symmetric(
+                  horizontal: horizontalPadding,
+                  vertical: verticalPadding,
+                ),
+                child: const _Header(),
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Text(
+                          "New Place 으으으",
+                          style: TextStyle(fontSize: 48.0, color: Colors.white),
+                        ),
+                      ),
+                      Expanded(
+                          flex: 1,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "더 많은 기회를 노린다.",
+                                style: TextStyle(
+                                    fontSize: 16.0, color: Colors.white),
+                              ),
+                              const SizedBox(height: 24.0),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text("자세히 보기"),
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.black),
+                              )
+                            ],
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                padding: EdgeInsets.symmetric(
+                    horizontal: horizontalPadding, vertical: verticalPadding),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: const SizedBox(),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            color: Colors.white,
+                            size: 16.0,
+                          ),
+                          const SizedBox(
+                            width: 8.0,
+                          ),
+                          Text(
+                            "대구광역시 달서구 호림동",
+                            style:
+                                TextStyle(fontSize: 16.0, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
 
